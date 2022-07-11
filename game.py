52415da2961b66d35662d6292eab889d5467b237
elenaaa234 <66561712+elenaaa234@users.noreply.github.com>
2022-07-11 19:53:57 +0100

    modification

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ health = 10
 if age >= 18:
   print ("You are old enough to play!")
 
-  wants_to_play = input("Do you want to play? ").lower()
+  wants_to_play = input("Do you want to play (yes/no)? ").lower()
   if wants_to_play =="yes":
     print("You are starting with", health, "health")
     print("Let's play!")
